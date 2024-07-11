d9d52995574cec524a81477d4ed3b26b0101d443
stevenshow <steven.schoebinger@gmail.com>
2024-07-10 23:16:42 -0600

    shrink form side nav and use hover card

--- src/app/(dashboard)/Sidebar.tsx
+++ src/app/(dashboard)/Sidebar.tsx
@@ -1,9 +1,14 @@
 'use client'
 
-import React, { useEffect } from 'react'
+import React from 'react'
 import { cn } from '@/lib/utils'
 import Link from 'next/link'
 import { useParams } from 'next/navigation'
+import {
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger
+} from '@/components/ui/hover-card'
 
 // Dummy data, normally comes from API
 let listOfForms = [
@@ -40,7 +45,7 @@ let listOfForms = [
 function ListIcon() {
     return (
         <div className="w-6 h-6 rounded-full bg-gray-200">
-            {/* <Image src='/icons/form_icon.png' alt='form icon'} />     */}
+            {/* <Image src='/icons/form_icon.png' alt='form icon'} /> */}
         </div>
     )
 }
@@ -55,31 +60,39 @@ function ListItem({
     selected_form_id: string
 }) {
     return (
-        <Link href={'/builder/' + form_id}>
-            <li
-                className={cn(
-                    'group cursor-pointer hover:border-primary hover:text-primary mt-3 py-2 px-3 flex gap-2 group shadow-md border rounded-xl',
-                    selected_form_id === form_id &&
-                        'border-primary text-primary font-bold'
-                )}
-            >
-                <ListIcon />
-                <span className="text-sm truncate">{form_title}</span>
-            </li>
-        </Link>
+        <HoverCard openDelay={100} closeDelay={100}>
+            <HoverCardTrigger asChild>
+                <Link href={'/builder/' + form_id}>
+                    <li
+                        className={cn(
+                            'group cursor-pointer hover:border-primary hover:text-primary mt-3 py-2 px-3 flex gap-3 shadow-md border rounded-xl md:rounded-full md:items-center md:text-center md:p-2',
+                            selected_form_id === form_id &&
+                                'border-primary text-primary font-bold'
+                        )}
+                    >
+                        <ListIcon />
+                        <span className="text-sm truncate hidden md:block">
+                            {form_title}
+                        </span>
+                    </li>
+                </Link>
+            </HoverCardTrigger>
+            <HoverCardContent side="right">{form_title}</HoverCardContent>
+        </HoverCard>
     )
 }
 
-export default function Sidebar({ className }: { className?: String }) {
+export default function Sidebar({ className }: { className?: string }) {
     const params = useParams()
 
     return (
         <nav className={cn('m-5 p-4 rounded-xl flex flex-col', className)}>
             <div className="flex justify-between">
                 <h1 className="font-bold text-sm ml-2 text-gray-600">
-                    My Forms
+                    <span className="block md:inline">My</span>
+                    <span className="hidden md:inline">&nbsp;</span>
+                    <span className="block md:inline">Forms</span>
                 </h1>
-                <button className="w-6 h-6 rounded-full bg-primary shadow-md border"></button>
             </div>
 
             <ul className="grow mt-4 overflow-auto scrollbar-hide">
