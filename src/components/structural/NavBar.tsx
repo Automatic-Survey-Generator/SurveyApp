37ed6f6cbfe93ad6fda19672af1e2685312900b4
stevenshow <steven.schoebinger@gmail.com>
2024-07-10 22:51:06 -0600

    remove all caps

--- src/components/structural/NavBar.tsx
+++ src/components/structural/NavBar.tsx
@@ -131,7 +131,7 @@ export default function NavBar() {
                     href="/"
                     className="text-primary text-2xl font-bold uppercase mr-auto"
                 >
-                    SURVEY CREATOR
+                    Survey Creator
                 </Link>
                 <nav className="hidden flex-col gap-6 font-medium md:flex md:flex-row md:items-center md:gap-5 lg:gap-6">
                     <div className="flex gap-6">
