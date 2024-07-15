d7d7e4be6fb3b9ef1c1a88a52d5cc1aef5d1d475
Ankush Jha <67179973+CR4ZED@users.noreply.github.com>
2024-07-15 19:37:55 +0530

    feat: Added middleware for protecting routes with authentication

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,5 +1,10 @@
 import { NextResponse } from 'next/server';
 
-export async function GET() {
+async function getTest(req) {
+  // this returns undefined because the request object here is different, 
+  // to get the user info we could make use of getServerSession()
+  console.log(req.user)
   return NextResponse.json({ message: 'Test' });
 }
+
+export { getTest as GET}
